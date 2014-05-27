18ef008918a2c9ad346f21d9144a475161939efd
minix <mgtuiw@gmail.com>
2014-05-28 00:23:26 +0800

    grid story image

--- main.rb
+++ main.rb
@@ -5,10 +5,38 @@ require 'sinatra/flash'
 require 'logger'
 require 'yaml'
 require_relative 'model/mongodb'
+require 'mongoid'
+require 'mongo'
+Mongoid.load!("config/mongodb.yml")
+
+include Mongo
+
+host = ENV['MONGO_RUBY_DRIVER_HOST'] || 'localhost'
+port = ENV['MONGO_RUBY_DRIVER_PORT'] || MongoClient::DEFAULT_PORT
+puts "Connecting to #{host}:#{port}"
+db = MongoClient.new(host, port).db('images')
+grid = Grid.new(db)
+
+@@files_collection = db.collection("fs.files") 
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + "/model")
 require 'user'
 
+def file_download(filename)
+	# This helper function is used to download files with and without extensions
+	# Look in GridFS for the given filename, sorting by the uploadDate
+	cursor = @@files_collection.find({"filename" => filename}, {:sort => ["uploadDate", :desc]})
+	if cursor.count > 1
+		response_string = "There are #{cursor.count} files that have the name: #{filename}.<br> They are listed below along with the dates on which they were uploaded. Click any of the links to download the file.<br>"
+		cursor.each do |file_item|
+			response_string = response_string + "<a href=\"#{request.base_url}/download/id/#{file_item["_id"].to_s}\">Date: #{file_item["uploadDate"].to_s} &nbsp;&nbsp; Filename: #{filename}</a><br>"
+		end
+	else
+		# Since there is only one file, redirect so that they get the download.
+		redirect "/download/id/#{cursor.first["_id"].to_s}"
+	end
+	response_string
+end
 
 configure do
 	enable :sessions, :logging
@@ -55,9 +83,9 @@ get "/upload" do
 	erb :upload
 end
 
-get '/image/:id' do
-	@image = Image.find(params[:id])
-	erb :show
+get "/images/:id" do |id|
+	@image = Image.where(:id => id).first()
+	erb :view
 end
 
 post "/signup" do
@@ -83,13 +111,35 @@ post "/login" do
 end
 
 post "/upload" do
-	@image = Image.create(:image => params[:image][:uploaded_photo])
-	@image.file_name = params[:image][:name]
-	@image.save
-	redirect '/'
+	@image = grid.put(params[:image][:tempfile].read)
+	#@image = Image.create(:image => params[:image][:uploaded_photo])
+	#@image.file_name = params[:image][:name]
+	return "Image Successfully upload! id=#{$id}"
+	#redirect '/'
 end
 
+get "/most_recent_upload" do
+	# Get the file handle from gridfs
+	file = grid.get($id)
+	# Set the content type to generic
+	content_type 'binary/octet-stream'
+	#Push the file at the browser
+	attachment "#{file.filename}"
+	response.write file.read
+end
 
+get '/download/:filename' do |filename|
+	  file_download(filename)
+end
+
+# Get a file by a specific file id
+get '/download/id/:file_id' do |file_id|
+	file = grid.get(BSON::ObjectId("#{file_id}"))
+	content_type 'binary/octet-stream'
+	#Push the file at the browser
+	attachment "#{file.filename}"
+	response.write file.read
+end
 
 __END__
 
